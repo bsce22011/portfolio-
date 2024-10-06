30021272dd9498d23879adce4721b6a560dbb857
Fatima Waseem <112568389+bsce22011@users.noreply.github.com>
2024-10-06 05:34:02 +0500

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,27 @@
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <h3>Contact Us</h3>
+                <p>Email: contact@example.com</p>
+                <p>Phone: +123 456 789</p>
+            </div>
+            <div class="col-md-4">
+                <h3>Follow Us</h3>
+                <a href="#"><i class="fab fa-facebook"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+            </div>
+            <div class="col-md-4">
+                <h3>Newsletter</h3>
+                <p>Subscribe to our newsletter to receive updates and offers!</p>
+                <form>
+                    <div class="form-group">
+                        <input type="email" class="form-control" placeholder="Enter your email">
+                    </div>
+                    <button type="submit" class="slay">Subscribe</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ header.php
@@ -0,0 +1,73 @@
+
+<div class="nav-bar-container">
+  <a href="/" class="nav-logo-container w-nav-brand">
+    <img src="https://assets-global.website-files.com/652a81c5d2f516632fee0f84/652a81c5d2f516632fee0fb5_logo-preload.svg" loading="lazy" width="Auto" alt="logo" class="nav-logo">
+  </a>
+  <div class="nav-menu-links">
+    <a href="/" class="nav-menu-link w-inline-block">
+      <div data-w-id="638e8a83-a803-4327-3c71-6f1e736c8e16" class="animation-rotate-text-container">
+        <div class="animation-rotate-text-one" style="transform: translate3d(0px, 0%, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;">
+          <div class="nav-menu-text">Home</div>
+        </div>
+        <div class="animation-rotate-text-two" style="transform: translate3d(0px, 0%, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;">
+          <div data-w-id="638e8a83-a803-4327-3c71-6f1e736c8e1b" class="nav-menu-text">Home<br></div>
+        </div>
+      </div>
+    </a>
+    <a href="/#projects" class="nav-menu-link w-inline-block">
+      <div data-w-id="638e8a83-a803-4327-3c71-6f1e736c8e1f" class="animation-rotate-text-container">
+        <div class="animation-rotate-text-one" style="transform: translate3d(0px, 0%, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;">
+          <div class="nav-menu-text">Projects</div>
+        </div>
+        <div class="animation-rotate-text-two" style="transform: translate3d(0px, 0%, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;">
+          <div class="nav-menu-text">Projects</div>
+        </div>
+      </div>
+    </a>
+    <a href="/about" class="nav-menu-link w-inline-block">
+      <div data-w-id="638e8a83-a803-4327-3c71-6f1e736c8e27" class="animation-rotate-text-container">
+        <div class="animation-rotate-text-one" style="transform: translate3d(0px, 0%, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;">
+          <div class="nav-menu-text">About</div>
+        </div>
+        <div class="animation-rotate-text-two" style="transform: translate3d(0px, 0%, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;">
+          <div class="nav-menu-text">About</div>
+        </div>
+      </div>
+    </a>
+    <a href="/blog" class="nav-menu-link w-inline-block">
+      <div data-w-id="638e8a83-a803-4327-3c71-6f1e736c8e2f" class="animation-rotate-text-container">
+        <div class="animation-rotate-text-one" style="transform: translate3d(0px, 0%, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;">
+          <div class="nav-menu-text">Blog</div>
+        </div>
+        <div class="animation-rotate-text-two" style="transform: translate3d(0px, 0%, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;">
+          <div class="nav-menu-text">Blog</div>
+        </div>
+      </div>
+    </a>
+    <a href="/contact" aria-current="page" class="nav-menu-link w-inline-block w--current">
+      <div data-w-id="638e8a83-a803-4327-3c71-6f1e736c8e37" class="animation-rotate-text-container">
+        <div class="animation-rotate-text-one" style="transform: translate3d(0px, 0%, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;">
+          <div class="nav-menu-text">Contact</div>
+        </div>
+        <div class="animation-rotate-text-two" style="transform: translate3d(0px, 0%, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;">
+          <div class="nav-menu-text">Contact</div>
+        </div>
+      </div>
+    </a>
+  </div>
+  <a href="tel:+00000000" class="nav-menu-link phone w-inline-block">
+    <div data-w-id="638e8a83-a803-4327-3c71-6f1e736c8e3f" class="animation-rotate-text-container">
+      <div class="animation-rotate-text-one" style="transform: translate3d(0px, 0%, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 
+
+0deg); transform-style: preserve-3d;">
+        <div class="nav-menu-text">+34 912 123 123</div>
+      </div>
+      <div class="animation-rotate-text-two" style="transform: translate3d(0px, 0%, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;">
+        <div class="nav-menu-text">+34 912 123 123</div>
+      </div>
+    </div>
+  </a>
+  <div data-w-id="638e8a83-a803-4327-3c71-6f1e736c8e46" class="nav-menu-mobile">
+    <img src="https://assets-global.website-files.com/652a81c5d2f516632fee0f84/652a81c5d2f516632fee0fd8_menu-alt-3.svg" loading="lazy" alt="">
+  </div>
+</div>
